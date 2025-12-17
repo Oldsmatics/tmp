6000416369d9e015a56a74afc17b748b4f935f46
Miriam Šventová <miriam.sventova@datasentics.com>
2025-12-17 12:03:53 +0100

    Adding py file

---
+++ run_code.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def run_code():
+    # Example code that performs a simple computation
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+    result = a + b
+    print("Result of addition:", result)
+
+if __name__ == "__main__":
+    run_code()
+
+
